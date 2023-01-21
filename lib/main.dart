70d46ef3337c5323cd5bf84c7738acbeed829b54
Purvesh Dongarwar <do.psd23@gmail.com>
2023-01-21 15:36:58 +0530

    arranged files added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
-import 'package:care_connect/catDetailsPage.dart';
+import 'package:care_connect/screens/catDetailsPage.dart';
+import 'package:care_connect/firebase_options.dart';
 import 'package:care_connect/homePage.dart';
 import 'package:care_connect/loginPage.dart';
+import 'package:care_connect/screens/homePage.dart';
+import 'package:care_connect/screens/loginPage.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     title: 'CareConnect',
@@ -13,9 +19,9 @@ void main() {
       useMaterial3: true,
     ),
     routes: {
-      "/": (context) => LoginPage(),
-      "/homepage": (context) => HomePage(),
-      "/details": (context) => Details(),
+      "/": (context) => const LoginPage(),
+      "/homepage": (context) => const HomePage(),
+      "/details": (context) => const Details(),
     },
   ));
 }
--- lib/screens/loginPage.dart
+++ lib/screens/loginPage.dart
@@ -1,4 +1,4 @@
-import 'package:care_connect/constants.dart';
+import 'package:care_connect/constants/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
